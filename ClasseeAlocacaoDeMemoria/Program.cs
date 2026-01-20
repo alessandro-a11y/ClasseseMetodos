c27d75caaa4e6ef515e27ec57f080c982dd6e38b
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-19 21:05:42 -0300

    Create Program.cs

---
+++ ClasseeAlocacaoDeMemoria/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ClasseeAlocacaoDeMemoria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Alocacao();
+        }
+        public static void Alocacao()
+        {
+            Console.WriteLine("Olá");
+        }
+    }
+}
