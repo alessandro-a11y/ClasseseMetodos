65385480b3a26ffb6b47d4027409d8f6ba122c82
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-21 10:19:06 -0300

    Create Program.cs

---
+++ Construtor1e2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Construtor1e2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno("Alessandro");
+            Aluno aluno2 = new Aluno( 18, "Masculino", "Sim");
+
+            Console.WriteLine(aluno.Nome == null ? "null" : aluno.Nome);
+            Console.WriteLine(aluno.Idade == 0 ? "não há valor" : aluno2.Idade);
+            Console.WriteLine(aluno.Sexo == null ? "null" : aluno2.Sexo);
+            Console.WriteLine(aluno.Aprovado == null ? "null" : aluno2.Aprovado);
+        }
+    }
+    public class Aluno
+    {
+        public Aluno(string nome)
+        {
+          Nome = nome;
+        }
+        public Aluno(int idade, string sexo, string aprovado)
+        {
+            Idade = idade;
+            Sexo = sexo;
+            Aprovado = aprovado;
+        }
+        public string? Nome;
+        public int Idade;
+        public string? Sexo;
+        public string? Aprovado;
+    }
+}
