d1bee41de9a23befbf15743a531565a1512ae61c
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-21 10:18:51 -0300

    Update Metodos.AssemblyInfo.cs

--- Metodos/obj/Debug/net10.0/Metodos.AssemblyInfo.cs
+++ Metodos/obj/Debug/net10.0/Metodos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Metodos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+783dd101578c71aa5460e9d6bf7e5f6b6e93de79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f05c920b650780a3bf7764850559ffb3604496b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Metodos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Metodos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
