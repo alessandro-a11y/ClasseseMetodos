e8fb7cce6bea8b3868786526ccdc6b38df989b59
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-21 10:35:24 -0300

    Update Program.cs

--- Construtor1e2/Program.cs
+++ Construtor1e2/Program.cs
@@ -1,34 +1,36 @@
 ﻿using System;
+
 namespace Construtor1e2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Aluno aluno = new Aluno("Alessandro");
-            Aluno aluno2 = new Aluno( 18, "Masculino", "Sim");
+            Aluno aluno = new Aluno("Alessandro", 18, "Masculino", "Sim");
 
-            Console.WriteLine(aluno.Nome == null ? "null" : aluno.Nome);
-            Console.WriteLine(aluno.Idade == 0 ? "não há valor" : aluno2.Idade);
-            Console.WriteLine(aluno.Sexo == null ? "null" : aluno2.Sexo);
-            Console.WriteLine(aluno.Aprovado == null ? "null" : aluno2.Aprovado);
+            Console.WriteLine(aluno.Nome ?? "null");
+            Console.WriteLine(aluno.Idade == 0 ? "não há valor" : aluno.Idade);
+            Console.WriteLine(aluno.Sexo ?? "null");
+            Console.WriteLine(aluno.Aprovado ?? "null");
         }
     }
+
     public class Aluno
     {
-        public Aluno(string nome)
-        {
-          Nome = nome;
-        }
-        public Aluno(int idade, string sexo, string aprovado)
+        public string? Nome;
+        public int Idade;
+        public string? Sexo;
+        public string? Aprovado;
+
+        // Construtor 1
+        public Aluno(string nome) => Nome = nome;
+        // Construtor 2 (encadeado)
+        public Aluno(string nome, int idade, string sexo, string aprovado)
+            : this(nome)
         {
             Idade = idade;
             Sexo = sexo;
             Aprovado = aprovado;
         }
-        public string? Nome;
-        public int Idade;
-        public string? Sexo;
-        public string? Aprovado;
     }
 }
