7b38fbeb3bd45cc7831c58fd49f194b82f6f043c
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-21 10:18:31 -0300

    Update Program.cs

--- Metodos/Program.cs
+++ Metodos/Program.cs
@@ -1,33 +1,33 @@
-﻿using System;
-namespace Metodos
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            MinhaClasse function = new MinhaClasse();
-            function.MeuMetodo();
-            Console.WriteLine(function);
-            MinhaClasse date = new MinhaClasse();
-            date.DataAtual();
-            Console.WriteLine(date);
+﻿//using System;
+//namespace Metodos
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            MinhaClasse function = new MinhaClasse();
+//            function.MeuMetodo();
+//            Console.WriteLine(function);
+//            MinhaClasse date = new MinhaClasse();
+//            date.DataAtual();
+//            Console.WriteLine(date);
 
-        }
-        class MinhaClasse
-        {
-            public void MeuMetodo()
-            {
-                Console.WriteLine("Hello, World");
-            }
+//        }
+//        class MinhaClasse
+//        {
+//            public void MeuMetodo()
+//            {
+//                Console.WriteLine("Hello, World");
+//            }
 
-            public void DataAtual()
-            {
-                Console.WriteLine(DateTime.Now);
-            }
-            public override string ToString()
-            {
-                return "Esta é uma instância da classe MinhaClasse";
-            }
-        }
-    }
-}
+//            public void DataAtual()
+//            {
+//                Console.WriteLine(DateTime.Now);
+//            }
+//            public override string ToString()
+//            {
+//                return "Esta é uma instância da classe MinhaClasse";
+//            }
+//        }
+//    }
+//}
