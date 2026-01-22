54083a2d7145cb5000ccebe1886c90a3031a9680
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-22 10:47:00 -0300

    Update Program.cs

--- ProjetoCarroConsole/Program.cs
+++ ProjetoCarroConsole/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
+using System.Runtime.Intrinsics.X86;
 namespace Metodos
 {
     internal class Program 
     {
         static void Main(string[] args)
         {
-            Car chevrolet = new Car("Sedan", "Chevrolet", "Onix", 2016, 110);
-            Car ford = new Car("SUV", "Ford", "EcoSport", 2018, 130);
+
 
             //Car chevrolet = new Car();
             //chevrolet.Model = "Sedan";
@@ -14,7 +14,7 @@ namespace Metodos
             //chevrolet.Mark = "Onix";
             //chevrolet.Year = 2016;
             //chevrolet.Power = 110;
-            
+
 
 
             //Car ford = new Car();
@@ -23,13 +23,32 @@ namespace Metodos
             //ford.Mark = "EcoSport";
             //ford.Year = 2018;
             //ford.Power = 130;
-            
 
-            Console.WriteLine($"Modelo: {chevrolet.Model}\nMontadora: {chevrolet.MarManufacturer}\nMarca: {chevrolet.Mark}\nYear: {chevrolet.Year}\nPotência: {chevrolet.Power}");
-            Console.WriteLine($"\nModelo: {ford.Model}\nMontadora: {ford.MarManufacturer}\nMarca: {ford.Mark}\nYear: {ford.Year}\nPotência: {ford.Power}\n");
+            Car chevrolet = new Car("Sedan", "Chevrolet", "Onix", 2016, 110);
+            Car ford = new Car("SUV", "Ford", "EcoSport", 2018, 130);
+            // chamar o metodo velocidade maxima
+           
+
+            Console.WriteLine(
+                $"Modelo: {chevrolet.Model}\n" +
+                $"Montadora: {chevrolet.MarManufacturer}\n" +
+                $"Marca: {chevrolet.Mark}\n" +
+                $"Ano: {chevrolet.Year}\n" +
+                $"Potência: {chevrolet.Power}\n"
+                );
+            
+            Console.WriteLine($"" +
+                $"Modelo: {ford.Model}\n" +
+                $"Montadora: {ford.MarManufacturer}\n" +
+                $"Marca: {ford.Mark}\n" +
+                $"Ano: {ford.Year}\n" +
+                $"Potência: {ford.Power}\n"
+                );
+            
             chevrolet.Accelerating(chevrolet.Mark);
             ford.Accelerating(ford.Mark);
-
+            Console.WriteLine($"\nA velocidade máxima do Onix é : {chevrolet.VelocidadeMaxima(chevrolet.Power)}km/h");
+            Console.WriteLine($"\nA velocidade máxima do EcoSport é : {ford.VelocidadeMaxima(ford.Power)}km/h");
         }
     }
     public class Car
@@ -39,15 +58,26 @@ namespace Metodos
         public string? Mark;
         public int Year;
         public int Power;
-
+        //Crie um construtor que permita criar um carro somente usando os atributos : Modelo e Montadora (não 
+        //use a palavra-chave this)
+        public int VelocidadeMaxima(int potencia)
+        {
+            double velMax = potencia * 1.75;
+            return (int)velMax;
+        }
+        public Car(string model, string manufacturer)
+        {
+            Model = model;
+            MarManufacturer = manufacturer;
+        }
         // Construtor
         public Car(string model, string manufacturer, string mark, int year, int power)
         {
-            this.Model = model;
-            this.MarManufacturer = manufacturer;
-            this.Mark = mark;
-            this.Year = year;
-            this.Power = power;
+            Model = model;
+            MarManufacturer = manufacturer;
+            Mark = mark;
+            Year = year;
+            Power = power;
         }
 
 
