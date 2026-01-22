e6a4886caf4b42e44f621fa3c8d471b48aea4453
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-22 11:56:39 -0300

    Update Objetos.AssemblyInfo.cs

--- Objetos/obj/Debug/net10.0/Objetos.AssemblyInfo.cs
+++ Objetos/obj/Debug/net10.0/Objetos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Objetos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31f76924bfc999544210a032abbf1c2520952b55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc6484266610c648526003a7cad2639809ba25bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Objetos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Objetos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
