8cdfd62a96a3dec6413e7a6402459b61b26e2ae2
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-25 17:07:25 -0300

    Update ProjetoCarroConsole.AssemblyInfo.cs

--- ProjetoCarroConsole/obj/Debug/net10.0/ProjetoCarroConsole.AssemblyInfo.cs
+++ ProjetoCarroConsole/obj/Debug/net10.0/ProjetoCarroConsole.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoCarroConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bba1337d61cffd05332ae97bf88afd64c6ff2e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f9a09c7c069940e5185fae98555d24e9e0371d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoCarroConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoCarroConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
