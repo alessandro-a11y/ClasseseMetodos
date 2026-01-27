79329385a10964828fa65d31fde216bb66182998
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-26 22:15:27 -0300

    Create Program.cs

---
+++ ParametrosOpcionais/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ParametrosOpcionais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Email email = new Email();
+            email.Enviar("figueiredoalessandro73@gmail.com");
+        }
+    }
+    public class Email
+    {
+        public void Enviar(string destino, string titulo = "Reunião", string assunto = "avalição técnica")
+        {
+            Console.WriteLine($"{destino}\t{titulo}\t {assunto}");
+        }
+    }
+}
