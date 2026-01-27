044184386825f251587e6193b3f59207d18aa271
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-26 22:19:01 -0300

    Update Program.cs

--- ArgumentosNomeados/Program.cs
+++ ArgumentosNomeados/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace ArgumentosNomeados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Email email = new Email();
+            email.Enviar(titulo: "Demissão",
+                assunto:"Demissão por causa de insultos",
+                destino: "figueiredoalessandro73@gmail.com"
+                ); 
+        }
+    }
+    public class Email
+    {
+        public void Enviar(string destino, string titulo, string assunto)
+        {
+            Console.WriteLine($"Email:{destino}\nTítulo: {titulo}\nAssunto: {assunto}");
+        }
+    }
+}
