cde8c2c230ea46fcdeb2b10d404dbc47251d637a
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-29 10:33:45 -0300

    Update Program.cs

--- ConstrutorStatico/Program.cs
+++ ConstrutorStatico/Program.cs
@@ -10,27 +10,10 @@ namespace ConstrutorStatic
             Console.WriteLine($"{p1.Nome} - {p1.idade}");
             Console.WriteLine($"Idade mínima: {Pessoa.IdadeMinima}");
 
-        }
-    }
-    public class Pessoa
-    {
-        public static int IdadeMinima;
-        public string Nome { get; set; } = string.Empty;
-        public int idade { get; set; }
+            Pessoa p2 = new Pessoa("Alessandro", 18);
+            Console.WriteLine($"{p2.Nome} - {p2.idade}");
+            Console.WriteLine($"Idade mínima: {Pessoa.IdadeMinima}");
 
-        public Pessoa(string nome, int idade)
-        {
-            Console.WriteLine("\nExecutando o construtor parametrizado");
-            Nome = nome;
-            this.idade = idade;
-        }
-        public Pessoa()
-        { }
-        static Pessoa()
-        {
-            Console.WriteLine("\nExecutando o construtor estático");
-            Console.WriteLine("Inicializando o campo IdadeMinima");
-            IdadeMinima = 18;
         }
     }
 }
